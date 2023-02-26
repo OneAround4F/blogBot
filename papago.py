338484747447af42e4873d426d2b51f8f3e950fc
OneAround4F <124264559+OneAround4F@users.noreply.github.com>
2023-02-26 17:21:27 +0900

    Update papago.py

--- papago.py
+++ papago.py
@@ -42,5 +42,4 @@ def run_translate(client_id: str, client_secret: str, english_text: str) -> str:
         print("Error Code : " + rescode)
 
 # Test Code - 영어 문장을 한글로 번역
-# source = "I go to work every day."
-# print(run_translate(client_key[0]['client_id'], client_key[0]['client_secret'], source))
+# print(run_translate(client_key[0]['client_id'], client_key[0]['client_secret'], "I go to work every day."))
